1ec5dd5d0f09a9b85d2b664e424578454ebd688e
skronschlaeger16 <sebkro90@gmail.com>
2020-07-08 18:15:14 +0200

    Register (LOGIN) Dtos (Requests, Response), 1. Layout

--- Webshop.Shared/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Webshop.Shared
-{
-    public class Class1
-    {
-    }
-}
---
+++ Webshop.Shared/Models/Responses/RegisterResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Webshop.Shared.Models.Responses
+{
+   public class RegisterResponse
+    {
+        public string Status { get; set; }
+        public string[] Data { get; set; }
+    }
+}
